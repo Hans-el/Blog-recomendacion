db9312cf20f2b8622832301cac8d4026ca7b4c65
Hans-el <halcivar3030@utm.edu.ec>
2025-07-12 09:21:58 -0500

    Primer commit del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaginaprincipalComponent } from './componentes/paginaprincipal/paginaprincipal.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { BlogPostComponent } from './componentes/blog-post/blog-post.component';
+import { ProductComponent } from './componentes/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaprincipalComponent,
+    AboutComponent,
+    BlogPostComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
